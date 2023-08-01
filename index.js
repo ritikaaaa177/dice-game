157fc51e1aadbe3fb86c0b38a0c4bd9c9b8b4d81
Ritika Kumar <kumarritika666@gmail.com>
2023-08-02 03:14:00 +1400

    fifth commit

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const score1 = document.getElementById("score--1");
 const diceel = document.querySelector(".dice");
 const rolldice = document.querySelector(".btn--roll");
 const player1currentscore = document.getElementById("current--0");
+const player2currentscore = document.getElementById("current--1");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 
 let currentscore = 0;
-
+let scores = [0, 0];
+let activeplayer = 0;
 score0.textContent = 0;
 score1.textContent = 0;
 
@@ -22,9 +26,17 @@ rolldice.addEventListener("click", function () {
 
   if (dicenumber != 1) {
     currentscore += dicenumber;
-    player1currentscore.textContent = currentscore;
+    document.getElementById(`current--${activeplayer}`).textContent =
+      currentscore;
+
+    // player1currentscore.textContent = currentscore;
     console.log(player1currentscore.textContent);
   } else {
+    document.getElementById(`current--${activeplayer}`).textContent = 0;
+    currentscore = 0;
+    activeplayer = activeplayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
   }
 });
 
